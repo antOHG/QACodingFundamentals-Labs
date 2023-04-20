d562a32ad6b9b950b357fca4c9f88ae041774265
antOHG <56338803+antOHG@users.noreply.github.com>
2023-04-20 14:40:18 +0100

    Add files via upload

---
+++ Lab1-p2.py
@@ -0,0 +1,13 @@
+username=input('Please enter your name: ')
+age = input('Please enter your age: ')
+print(username,'is',age,'years old: ')
+
+import math
+
+sideA=int(input("Side A:" ))
+sideB=int(input("Side B:" ))
+sideC=int(input("Side C:" ))
+
+print(sideA+sideB+sideC)
+print(sideA+sideC / 2)
+
---
+++ Lab1.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+
+username='Bob'
+age='32'
+print(username,'is '+age+' years old')
+print(username+' is '+age+' years old')
+
+print(username,' is '+age+' years old')
