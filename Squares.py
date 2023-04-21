8b4909655d682b47919dd400f28d3ba8091d675c
antOHG <56338803+antOHG@users.noreply.github.com>
2023-04-21 16:06:30 +0100

    Add files via upload
    Lab 2 work for 21/04/2023
    Lab 3 to follow

---
+++ Squares.py
@@ -0,0 +1,14 @@
+
+# Squares.py created by AM on 21/04/2023
+    
+# Chapter 03 Lab - Part 1
+# Using while loops
+# Task 1 - Squares
+# incomplete
+
+
+import math
+
+number = float(input("Enter a number to be squared: "))
+while number >1 and number <100:
+    print(number)
